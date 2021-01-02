3821f1b166b78e00949cdeb937ec31447f6eb8b1
wookyoungkim <viliketh1s98@naver.com>
2021-01-02 18:35:48 +0900

    프로젝트 생성 및 설정

---
+++ src/main/java/hello/hellospirng/HelloSpirngApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpirngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellospirng/HelloSpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpirngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
