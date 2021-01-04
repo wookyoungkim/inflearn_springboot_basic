66b60bba343ea9271dfe96cdb64f8bd3ecbc13e4
wookyoungkim <viliketh1s98@naver.com>
2021-01-04 17:54:40 +0900

    구현한 기능에 대한 테스트코드 작성

--- src/main/java/hello/hellospirng/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospirng/repository/MemoryMemberRepository.java
@@ -35,4 +35,9 @@ public class MemoryMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore(){
+        store.clear();
+    }
 }
+
