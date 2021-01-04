d26ba9779f71e97f005277d9ec819d74f650c703
wookyoungkim <viliketh1s98@naver.com>
2021-01-04 18:06:11 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/hellospirng/service/MemberService.java
@@ -0,0 +1,36 @@
+package hello.hellospirng.service;
+
+import hello.hellospirng.domain.Member;
+import hello.hellospirng.repository.MemberRepository;
+import hello.hellospirng.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    private Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
