dd5193e7ce5ef5181ca6463dc04b300c8cf8069d
wookyoungkim <viliketh1s98@naver.com>
2021-01-07 19:57:18 +0900

    자바 코드로 직접 의존관계 생성

--- src/main/java/hello/hellospirng/SpringConfig.java
+++ src/main/java/hello/hellospirng/SpringConfig.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    //스프링 빈에 등록하라는 어노테이션
-    // 뜰때 -> 멤버 서비스와 멤버 리포지토리 둘다 컨테이너에 등록
-    // 스프링 빈 등록되어 있는 멤버 리포지토리를 서비스에 넣어줌
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
