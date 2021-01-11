e3eff2eb5f4e0b8f2e4ce152ca12c04fe37b1405
wookyoungkim <viliketh1s98@naver.com>
2021-01-11 15:52:59 +0900

    회원 등록 기능 추가

--- src/main/java/hello/hellospirng/controller/MemberController.java
+++ src/main/java/hello/hellospirng/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospirng.controller;
 
+import hello.hellospirng.domain.Member;
 import hello.hellospirng.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        //MemberForm의 name에 html에서 받아온 name이 들어감
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        //메인으로 리다이렉트하기
+        return "redirect:/";
+
     }
 }
