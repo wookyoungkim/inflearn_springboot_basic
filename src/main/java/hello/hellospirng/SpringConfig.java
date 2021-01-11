7637fceab4fffb6225c0dbc9006bd759c6955ad9
wookyoungkim <viliketh1s98@naver.com>
2021-01-11 17:29:24 +0900

    H2 database를 이용한 순수 JDBC 구현 코드

--- src/main/java/hello/hellospirng/SpringConfig.java
+++ src/main/java/hello/hellospirng/SpringConfig.java
@@ -1,20 +1,33 @@
 package hello.hellospirng;
 
+import hello.hellospirng.repository.JdbcMemberRepository;
 import hello.hellospirng.repository.MemberRepository;
 import hello.hellospirng.repository.MemoryMemberRepository;
 import hello.hellospirng.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
